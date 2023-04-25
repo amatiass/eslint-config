69473bc9011055c5c2a56b00932bec497056fbaa
Amatias <48198890+amatiass@users.noreply.github.com>
2023-04-25 12:55:51 +0800

    feat: Change jsx rules. fix: Fix eslint style error.

--- packages/eslint-config-jsx/index.js
+++ packages/eslint-config-jsx/index.js
@@ -10,7 +10,6 @@ module.exports = {
   rules: {
     'react/jsx-uses-react': 'error',
     'react/jsx-uses-vars': 'error',
-    'react/no-unknown-property': [0, {}],
     'react/react-in-jsx-scope': [0, {}],
     'react/jsx-first-prop-new-line': [2, 'multiline'],
     'react/jsx-indent': [2, 2],
@@ -20,10 +19,10 @@ module.exports = {
     'react/jsx-curly-newline': [2, { multiline: 'consistent', singleline: 'consistent' }],
     'react/jsx-curly-spacing': [2, { when: 'never', children: true }],
     'react/jsx-equals-spacing': [2, 'never'],
-    'react/jsx-tag-spacing': [2, { beforeSelfClosing: 'proportional-always', beforeClosing: 'proportional-always' }],
-    'react/jsx-wrap-multilines': [2, { declaration: 'parens', assignment: 'parens', return: 'parens', arrow: 'parens', condition: 'parens', logical: 'parens', prop: 'parens' }],
+    'react/jsx-tag-spacing': [2, { beforeSelfClosing: 'always', beforeClosing: 'never' }],
     'react/jsx-max-props-per-line': [2, { when: 'multiline' }],
     'react/jsx-closing-bracket-location': [2, 'tag-aligned'],
-    'react/jsx-props-no-multi-spaces': [2]
+    'react/jsx-props-no-multi-spaces': [2],
+    'react/jsx-wrap-multilines': [2, { declaration: 'parens-new-line', assignment: 'parens-new-line', return: 'parens-new-line', arrow: 'parens-new-line', condition: 'parens-new-line', logical: 'parens-new-line', prop: 'parens-new-line' }]
   }
 }
